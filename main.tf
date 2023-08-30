b117995698938f9b52152b2f293d9250dd2e20ac
chinnapareddy <chinna.aduri@gmail.com>
2023-08-30 02:14:26 -0700

    subnet creation test

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ module "component" {
    source   = "git::https://github.com/chinna3107/tf-module-vpc.git"
     for_each = var.vpc
     cidr = each.value["cidr"]
+  tags = {
+    name = each.key
+  }
+
 }
