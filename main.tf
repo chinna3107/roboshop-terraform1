ef9b6e635113540e38bbb34ef1c4af525a8544b2
chinnapareddy <chinna.aduri@gmail.com>
2023-08-30 18:35:48 -0700

    Subnet creation

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "component" {
    source   = "git::https://github.com/chinna3107/tf-module-vpc.git"
     for_each = var.vpc
     cidr = each.value["cidr"]
+    subnets = each.value["subnets"]
  # tags = {
  #   name = each.key
  # }
